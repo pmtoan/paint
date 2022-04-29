702ea1e0b5bbd2de15afb0975a62e0027fbaed25
pmtoan <contact.pmtoan@gmail.com>
2022-04-30 01:04:22 +0700

    Ribbon UI

---
+++ Paint/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using IContract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Paint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // State
+        bool _isDrawing = false;
+        string _currentType = "";
+        IShapeEntity _preview = null;
+        Point _start;
+        List<IShapeEntity> _drawnShapes = new List<IShapeEntity>();
+
+        // Cấu hình
+        Dictionary<string, IPaintBusiness> _painterPrototypes = new Dictionary<string, IPaintBusiness>();
+        Dictionary<string, IShapeEntity> _shapesPrototypes = new Dictionary<string, IShapeEntity>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var exeFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var folderInfo = new DirectoryInfo(exeFolder);
+            var dllFiles = folderInfo.GetFiles("*.dll");
+
+            foreach (var dll in dllFiles)
+            {
+                Assembly assembly = Assembly.LoadFrom(dll.FullName);
+
+                Type[] types = assembly.GetTypes();
+
+                // Giả định: 1 dll chỉ có 1 entity và 1 business tương ứng
+                IShapeEntity? entity = null;
+                IPaintBusiness? business = null;
+
+                foreach (Type type in types)
+                {
+                    if (type.IsClass)
+                    {
+                        if (typeof(IShapeEntity).IsAssignableFrom(type))
+                        {
+                            entity = (Activator.CreateInstance(type) as IShapeEntity)!;
+                        }
+
+                        if (typeof(IPaintBusiness).IsAssignableFrom(type))
+                        {
+                            business = (Activator.CreateInstance(type) as IPaintBusiness)!;
+                        }
+                    }
+                }
+
+                //var img = new Bitmap
+                if (entity != null)
+                {
+                    _shapesPrototypes.Add(entity!.Name, entity);
+                    _painterPrototypes.Add(entity!.Name, business!);
+                }
+            }
+
+            Title = $"Tìm thấy {_shapesPrototypes.Count} hình";
+
+            BindingList<PluginItems> shapeItemSource = new BindingList<PluginItems>();
+
+            //// Tạo ra các nút bấm tương ứng
+            foreach (var (name, entity) in _shapesPrototypes)
+            {
+                shapeItemSource.Add(new PluginItems()
+                {
+                    PluginEntity = entity,
+                    PluginIconPath = "./images/"  + name + ".png",
+                });
+            }
+            shapeList.ItemsSource = shapeItemSource;
+
+            if (_shapesPrototypes.Count > 0)
+            {
+                //Lựa chọn nút bấm đầu tiên
+                var (key, shape) = _shapesPrototypes.ElementAt(0);
+                _currentType = key;
+                _preview = (shape.Clone() as IShapeEntity)!;
+            }
+        }
+
+        class PluginItems
+        {
+            public IShapeEntity PluginEntity { get; set; }
+            public string PluginIconPath { get; set; }
+        }
+
+        private void border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _isDrawing = true;
+            _start = e.GetPosition(canvas);
+
+            _preview.HandleStart(_start);
+        }
+
+        private void border_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDrawing)
+            {
+                var end = e.GetPosition(canvas);
+                _preview.HandleEnd(end);
+
+                // Xóa đi tất cả bản vẽ cũ và vẽ lại những đường thẳng trước đó
+                canvas.Children.Clear(); // Xóa đi toàn bộ
+
+                // Vẽ lại những hình đã vẽ trước đó
+                foreach (var item in _drawnShapes)
+                {
+                    IPaintBusiness painter = _painterPrototypes[item.Name];
+                    UIElement shape = painter.Draw(item);
+
+                    canvas.Children.Add(shape);
+                }
+
+                var previewPainter = _painterPrototypes[_preview.Name];
+                var previewElement = previewPainter.Draw(_preview);
+                canvas.Children.Add(previewElement);
+            }
+        }
+
+        private void border_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _isDrawing = false;
+
+            var end = e.GetPosition(canvas); // Điểm kết thúc
+
+            _preview.HandleEnd(end);
+
+            _drawnShapes.Add(_preview.Clone() as IShapeEntity);
+        }
+
+        private void chooseShapeBtnClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var entity = button!.Tag as IShapeEntity;
+
+            Debug.WriteLine(entity!.Name);
+
+            _currentType = entity!.Name;
+            _preview = (_shapesPrototypes[entity.Name].Clone() as IShapeEntity)!;
+        }
+    }
+}
